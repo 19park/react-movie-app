59b6b07bd3cc13a134f3fb77fcbc6df1edf0b3cb
19park <19park@wholedoc.net>
2019-01-29 13:50:51 +0900

    Updates

---
+++ precache-manifest.602c94d0ae5d1512ee915ee2720ec15d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdcfacafbc62971f5461",
+    "url": "/react-movie-app/static/js/runtime~main.bdcfacaf.js"
+  },
+  {
+    "revision": "0f904498c9be60ebf6aa",
+    "url": "/react-movie-app/static/js/main.0f904498.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/react-movie-app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "0f904498c9be60ebf6aa",
+    "url": "/react-movie-app/static/css/main.714f2fa2.chunk.css"
+  },
+  {
+    "revision": "78a092bfd9fffb36cc0b0428972360fd",
+    "url": "/react-movie-app/index.html"
+  }
+];
